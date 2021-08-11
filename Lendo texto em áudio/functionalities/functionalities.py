f9bbb304c11cbba405ed60d42611a36d7cbe7e9b
LEONARDO ALVES <52077278+Leon4rdoalves@users.noreply.github.com>
2021-08-10 22:40:18 -0300

    created the basis of project functions

--- Lendo texto em áudio/functionalities/functionalities.py
+++ Lendo texto em áudio/functionalities/functionalities.py
@@ -1,1 +1,21 @@
-...
+import PySimpleGUI as sg
+import os
+import speech_recognition as sr
+from gtts import gTTS
+from playsound import playsound
+
+
+def initWindow():
+    ...
+
+
+def windowSpeechRecognition():
+    ...
+
+
+def getAudio():
+    ...
+
+
+def speaking():
+    ...
