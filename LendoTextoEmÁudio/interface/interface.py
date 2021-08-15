832e62ccf76daaba1a2d487de27b5444c5783cba
LEONARDO ALVES <52077278+Leon4rdoalves@users.noreply.github.com>
2021-08-14 23:50:28 -0300

    Concluded
    Interface successfully created, file find and read functionalities working correctly. New features will be developed in the future.

--- LendoTextoEmÁudio/interface/interface.py
+++ LendoTextoEmÁudio/interface/interface.py
@@ -7,15 +7,12 @@ windowOne = initWindow()
 
 while True:
     window, event, values = sg.read_all_windows()
-
     if window == windowOne and event == 'Exit' or event == sg.WIN_CLOSED:
         break
-
     if window == windowOne and event == 'fileReading':
         filePath = values['-FILE-']
         fileName = os.path.basename(filePath)
         window['fileSelected'].update(f'lido → {fileName}', text_color='green')
-
         try:
             if fileName[-4:] == '.txt':
                 with open(filePath) as text_to_read:
@@ -25,8 +22,6 @@ while True:
                 window['fileSelected'].update('Ops, ainda não suporto este arquivo!', text_color='red')
         except:
             window['fileSelected'].update('Ops, este arquivo está vazio!', text_color='red')
-
         if filePath == '':
             window['fileSelected'].update('Hey, selecione um arquivo!', text_color='red')
-
 window.close()
