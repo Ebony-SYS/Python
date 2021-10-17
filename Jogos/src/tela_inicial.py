ee7349b903aad3a86cdd9c5e52fe94cbdcac14d8
leon4rdoalves <leon4rdoalvess@gmail.com>
2021-10-17 17:23:49 -0300

    dark theme

--- Jogos/src/tela_inicial.py
+++ Jogos/src/tela_inicial.py
@@ -1,3 +1,4 @@
+import tkinter
 from tkinter import *
 
 window = Tk()
@@ -11,6 +12,7 @@ class Aplication:
         self.frameFirstWindow()
         self.buttonFirstWindow()
         self.labelFirstWindow()
+        self.background()
         window.mainloop()
 
     def firstWindow(self):
@@ -22,37 +24,38 @@ class Aplication:
         self.window.minsize(width=450, height=450)
 
     def frameFirstWindow(self):
-        self.frame1 = Frame(self.window)
-        self.frame1.place(relx=0, rely=0, relwidth=1, relheight=0.07)
+        self.frame1 = Frame(self.window, background='#3a3b3c')
+        self.frame1.place(relx=0, rely=0, relwidth=1, relheight=0.06)
 
-        self.frame2 = Frame(self.window)
-        self.frame2.place(relx=0.01, rely=0.06, relwidth=0.98, relheight=0.88)
+        self.frame2 = Frame(self.window, background='#3a3b3c')
+        self.frame2.place(relx=0.01, rely=0.06, relwidth=0.98, relheight=0.90)
 
-        self.frame3 = Frame(self.window)
-        self.frame3.place(relx=0, rely=0.95, relwidth=1, relheight=0.05)
+        self.frame3 = Frame(self.window, background='#3a3b3c')
+        self.frame3.place(relx=0, rely=0.95, relwidth=1, relheight=0.07)
 
     def buttonFirstWindow(self):
-        self.btSnake = Button(self.frame2, text='Snake')
+        self.btSnake = Button(self.frame2, text='Snake', background='#3a3b3c')
         self.btSnake.place(relx=0.03, rely=0.03, relwidth=0.45, relheight=0.45)
 
-        self.btCampoMinado = Button(self.frame2, text='Campo Minado')
+        self.btCampoMinado = Button(self.frame2, text='Campo Minado', background='#3a3b3c')
         self.btCampoMinado.place(relx=0.52, rely=0.03, relwidth=0.45, relheight=0.45)
 
-        self.btCorrida = Button(self.frame2, text='Corrida')
+        self.btCorrida = Button(self.frame2, text='Corrida', background='#3a3b3c')
         self.btCorrida.place(relx=0.03, rely=0.52, relwidth=0.45, relheight=0.45)
 
-        self.btForca = Button(self.frame2, text='Forca')
+        self.btForca = Button(self.frame2, text='Forca', background='#3a3b3c')
         self.btForca.place(relx=0.52, rely=0.52, relwidth=0.45, relheight=0.45)
 
     def labelFirstWindow(self):
-        self.lbLogo = Label(self.frame1, text='ebony', font='abnes')
+        self.lbLogo = Label(self.frame1, text='ebony', font='abnes', background='#3a3b3c')
         self.lbLogo.place(relx=0.01, rely=0.2)
-        self.lbLogo1 = Label(self.frame1, text='sys', font='Debum 24')
+        self.lbLogo1 = Label(self.frame1, text='sys', font='Debum 24', background='#3a3b3c')
         self.lbLogo1.place(relx=0.16, rely=0.00)
-
-        self.lbRodape = Label(self.frame3, text='@ebony.programador', font='ubuntu 12')
+        self.lbRodape = Label(self.frame3, text='@ebony.programador', font='ubuntu 12', background='#3a3b3c')
         self.lbRodape.place(relx=0.76, rely=0.1)
 
+    def background(self):
+        ...
 
 
 Aplication()
