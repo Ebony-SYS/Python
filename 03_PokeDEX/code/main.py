767b7c76bdae34da0dabe31a641907138552ca0a
leon4rdoalves <leon4rdoalvess@gmail.com>
2022-10-26 03:49:32 -0300

    ready interface

--- 03_PokeDEX/code/main.py
+++ 03_PokeDEX/code/main.py
@@ -38,7 +38,7 @@ id.place(x=12, y=75)
 
 # Imagem
 imagem = Image.open('assets/img/pikachu.png')
-imagem = imagem.resize((238,238))
+imagem = imagem.resize((238, 238))
 imagem = ImageTk.PhotoImage(imagem)
 imagemBanner = Label(frameBanner, relief='flat', image=imagem)
 imagemBanner.place(x=60, y=50)
@@ -57,7 +57,8 @@ ataque = Label(window, text='Ataque: 200', relief='flat', anchor=CENTER, font=('
 ataque.place(x=15, y=385)
 defesa = Label(window, text='Defesa: 300', relief='flat', anchor=CENTER, font=('Verdana 10'), fg=azul, bg=branco)
 defesa.place(x=15, y=410)
-velocidade = Label(window, text='Velocidade: 400', relief='flat', anchor=CENTER, font=('Verdana 10'), fg=azul, bg=branco)
+velocidade = Label(window, text='Velocidade: 400', relief='flat', anchor=CENTER, font=('Verdana 10'), fg=azul,
+                   bg=branco)
 velocidade.place(x=15, y=435)
 total = Label(window, text='Total: 1000', relief='flat', anchor=CENTER, font=('Verdana 10'), fg=azul, bg=branco)
 total.place(x=15, y=460)
@@ -78,48 +79,57 @@ hab2.place(x=180, y=385)
 # Botões ________________________________________________________________↓
 
 imgPikachu = Image.open('assets/img/cabeca-pikachu.png')
-imgPikachu = imgPikachu.resize((40,40))
+imgPikachu = imgPikachu.resize((40, 40))
 imgPikachu = ImageTk.PhotoImage(imgPikachu)
-imagemBanner = Button(window, image=imgPikachu, text='Pikachu', font=('Verdana 12'),width=150, relief='raised', overrelief=RIDGE, compound=RIGHT, anchor=CENTER, padx=5, fg=preto, bg=branco)
+imagemBanner = Button(window, image=imgPikachu, text='Pikachu', font=('Verdana 12'), width=150, relief='raised',
+                      overrelief=RIDGE, compound=RIGHT, anchor=CENTER, padx=5, fg=preto, bg=branco)
 imagemBanner.place(x=375, y=10)
 
 imgBulbasaur = Image.open('assets/img/cabeca-bulbasaur.png')
-imgBulbasaur = imgBulbasaur.resize((40,40))
+imgBulbasaur = imgBulbasaur.resize((40, 40))
 imgBulbasaur = ImageTk.PhotoImage(imgBulbasaur)
-imagemBanner = Button(window, image=imgBulbasaur, text='Bulbasaur', font=('Verdana 12'),width=150, relief='raised', overrelief=RIDGE, compound=RIGHT, anchor=CENTER, padx=5, fg=preto, bg=branco)
+imagemBanner = Button(window, image=imgBulbasaur, text='Bulbasaur', font=('Verdana 12'), width=150, relief='raised',
+                      overrelief=RIDGE, compound=RIGHT, anchor=CENTER, padx=5, fg=preto, bg=branco)
 imagemBanner.place(x=375, y=65)
 
 imgCharmander = Image.open('assets/img/cabeca-charmander.png')
-imgCharmander = imgCharmander.resize((40,40))
+imgCharmander = imgCharmander.resize((40, 40))
 imgCharmander = ImageTk.PhotoImage(imgCharmander)
-imagemBanner = Button(window, image=imgCharmander, text='Charmander', font=('Verdana 12'),width=150, relief='raised', overrelief=RIDGE, compound=RIGHT, anchor=CENTER, padx=5, fg=preto, bg=branco)
+imagemBanner = Button(window, image=imgCharmander, text='Charmander', font=('Verdana 12'), width=150, relief='raised',
+                      overrelief=RIDGE, compound=RIGHT, anchor=CENTER, padx=5, fg=preto, bg=branco)
 imagemBanner.place(x=375, y=120)
 
 imgDragonite = Image.open('assets/img/cabeca-dragonite.png')
-imgDragonite = imgDragonite.resize((40,40))
+imgDragonite = imgDragonite.resize((40, 40))
 imgDragonite = ImageTk.PhotoImage(imgDragonite)
-imagemBanner = Button(window, image=imgDragonite, text='Dragonite', font=('Verdana 12'),width=150, relief='raised', overrelief=RIDGE, compound=RIGHT, anchor=CENTER, padx=5, fg=preto, bg=branco)
+imagemBanner = Button(window, image=imgDragonite, text='Dragonite', font=('Verdana 12'), width=150, relief='raised',
+                      overrelief=RIDGE, compound=RIGHT, anchor=CENTER, padx=5, fg=preto, bg=branco)
 imagemBanner.place(x=375, y=175)
 
 imgGengar = Image.open('assets/img/cabeca-gengar.png')
-imgGengar = imgGengar.resize((40,40))
+imgGengar = imgGengar.resize((40, 40))
 imgGengar = ImageTk.PhotoImage(imgGengar)
-imagemBanner = Button(window, image=imgGengar, text='Gengar', font=('Verdana 12'),width=150, relief='raised', overrelief=RIDGE, compound=RIGHT, anchor=CENTER, padx=5, fg=preto, bg=branco)
+imagemBanner = Button(window, image=imgGengar, text='Gengar', font=('Verdana 12'), width=150, relief='raised',
+                      overrelief=RIDGE, compound=RIGHT, anchor=CENTER, padx=5, fg=preto, bg=branco)
 imagemBanner.place(x=375, y=230)
 
 imgGyarados = Image.open('assets/img/cabeca-gyarados.png')
-imgGyarados = imgGyarados.resize((40,40))
+imgGyarados = imgGyarados.resize((40, 40))
 imgGyarados = ImageTk.PhotoImage(imgGyarados)
-imagemBanner = Button(window, image=imgGyarados, text='Gyarados', font=('Verdana 12'),width=150, relief='raised', overrelief=RIDGE, compound=RIGHT, anchor=CENTER, padx=5, fg=preto, bg=branco)
+imagemBanner = Button(window, image=imgGyarados, text='Gyarados', font=('Verdana 12'), width=150, relief='raised',
+                      overrelief=RIDGE, compound=RIGHT, anchor=CENTER, padx=5, fg=preto, bg=branco)
 imagemBanner.place(x=375, y=285)
 
 # Botões ________________________________________________________________↑
 # onclick ________________________________________________________________↓
 
 
-
-
 # onclick ________________________________________________________________↑
 
 
 window.mainloop()
+
+'''
+Desenvolvido por Leonardo Alves
+Contato: leon4rdoalvess@gmail.com
+'''
