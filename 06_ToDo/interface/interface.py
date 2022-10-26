ac9909dd2ea95423a9eaafe65601aa17b8521734
leon4rdoalves <leon4rdoalvess@gmail.com>
2022-10-26 12:22:59 -0300

    starting the project

---
+++ 06_ToDo/interface/interface.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+preto = '#000'
+branco = '#fff'
+vermelho = '#cc1d4e'
+amarelo = '#d1d420'
+verde = '#59b356'
+cinza = '#d9d9d9'
+dark = '#101010'
+dark1 = '#1e1e1e'
+
+janela = Tk()
+janela.resizable(width=FALSE, height=FALSE)
+janela.geometry('500x225')
+janela.title('Ebony Sys | Leonardo Alves → To-Do')
+janela.configure(background=dark)
+
+
+
+janela.mainloop()
+
