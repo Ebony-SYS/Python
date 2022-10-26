dac934c8145b7aecfbb3d7e778791341322a330c
leon4rdoalves <leon4rdoalvess@gmail.com>
2022-10-26 03:34:00 -0300

    up to all

---
+++ 01_TextoemAudio/interface/interface.py
@@ -0,0 +1,27 @@
+import PySimpleGUI as sg
+import os
+from inspect import Traceback
+from functionalities.functionalities import initWindow, Speaking
+
+windowOne = initWindow()
+
+while True:
+    window, event, values = sg.read_all_windows()
+    if window == windowOne and event == 'Exit' or event == sg.WIN_CLOSED:
+        break
+    if window == windowOne and event == 'fileReading':
+        filePath = values['-FILE-']
+        fileName = os.path.basename(filePath)
+        window['fileSelected'].update(f'lido → {fileName}', text_color='green')
+        try:
+            if fileName[-4:] == '.txt':
+                with open(filePath) as text_to_read:
+                    txt = text_to_read.read()
+                    Speaking(txt)
+            else:
+                window['fileSelected'].update('Ops, ainda não suporto este arquivo!', text_color='red')
+        except:
+            window['fileSelected'].update('Ops, este arquivo está vazio!', text_color='red')
+        if filePath == '':
+            window['fileSelected'].update('Hey, selecione um arquivo!', text_color='red')
+window.close()
---
+++ 04_PokeDEX/assets/dados/dados.py
@@ -0,0 +1,46 @@
+pokemons = {
+    'Pikachu': {
+        'status': [
+            'HP: 300', 
+            'ATAQUE: 600', 
+            'DEFESA: 500', 
+            'VELOCIDADE: 300',
+            'TOTAL: 1.700'
+            ],
+        'habilidades': [
+            'Estático', 
+            'Pára-raios'
+            ],
+        'categoria': [
+            '#025', 
+            'Elétrico', 
+            'assets/img/pikachu.png'
+            ],
+    },
+    
+    'Bulbasaur': {
+        'status': [],
+        'habilidades': [],
+        'categoria': [],
+    },
+    'Charmander': {
+        'status': [],
+        'habilidades': [],
+        'categoria': [],
+    },
+    'Dragonite': {
+        'status': [],
+        'habilidades': [],
+        'categoria': [],
+    },
+    'Gengar': {
+        'status': [],
+        'habilidades': [],
+        'categoria': [],
+    },
+    'Gyarados': {
+        'status': [],
+        'habilidades': [],
+        'categoria': [],
+    }
+}
