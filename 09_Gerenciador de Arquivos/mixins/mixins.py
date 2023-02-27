3cb67e1da5927b8a9cf456b1452bed94127a40bb
leon4rdoalves <leon4rdoalvess@gmail.com>
2023-02-27 16:04:48 -0300

    criada a função para encontrar e contar a quantidade de pastas de arquivos, iniciada a função que vai permitir escolher e renderizar estas informações. Os frames alocados no frame main foram criados e posicionados, seu conteúdo está dependendo da função de renderização, por isto ainda não foram todos pos criados e posicionados.

--- 09_Gerenciador de Arquivos/mixins/mixins.py
+++ 09_Gerenciador de Arquivos/mixins/mixins.py
@@ -1,3 +1,8 @@
+from tkinter import filedialog as fd
+import os
+import shutil
+
+# root cores
 preto = '#000'
 branco = '#fff'
 dark1 = '#3F3F3F'
@@ -13,14 +18,62 @@ violeta = '#9400f3'
 rosa = '#ff00ff'
 laranja = '#ff8c00'
 
-# root fonte
-fonte_principal = 'Ubuntu'
-cor_fonte_principal = '#f0f8ff'
-fonte_secundaria = ''
-cor_fonte_secundaria = '#ffd700'
-
 cores = [
     amarelo, azul_claro, azul_escuro,
     verde_claro, verde_escuro, vermelho,
     violeta, rosa, laranja
 ]
+
+# root fonte
+fonte_principal = 'Ubuntu 16 bold'
+cor_fonte_principal = '#f0f8ff'
+fonte_secundaria = 'Ubuntu 14'
+cor_fonte_secundaria = '#ffd700'
+
+# root imagens
+img_logo = '../assets/img/logo.png'
+icone_pasta = '../assets/img/pasta.png'
+
+global pasta
+
+
+# função para contar pastas
+def contarPastasArquivos(caminho, extencao):
+    qtd_pastas = 0
+    qtd_arquivos = 0
+
+    for item in os.listdir(caminho):
+        if os.path.isfile(os.path.join(caminho, item)) and item.endswith(extencao):
+            qtd_arquivos += 1
+        elif os.path.isdir(os.path.join(caminho, item)):
+            qtd_pastas += 1
+            subpasta_path = os.path.join(caminho, item)
+            qtd_subarquivos, qtd_subpastas = contarPastasArquivos(subpasta_path, extencao)
+            qtd_pastas += qtd_subpastas
+            qtd_arquivos += qtd_subarquivos
+
+    return qtd_pastas, qtd_arquivos
+
+
+# função para escolher uma pasta
+def escolherPasta():
+    global pasta
+    pasta = fd.askdirectory()
+
+    extencoes = ['.txt', '.csv', '.xlsx', '.pdf', '.json', '.jpg', '.jpeg', '.png', '.mpeg',
+                 '.mp3', '.mp4', '.avi', '.html', '.css', '.sass', '.js', '.py', '.sql', '.ty']
+
+    extencoes_combobox = []
+    extencoes_encontradas = []
+    extencoes_quantidade = []
+    quantidade_pastas = 0
+
+    for item in extencoes:
+        caminho = pasta
+        extencao = item
+        qtd_arquivos, qtd_pastas = contarPastasArquivos(caminho, extencao)
+
+        if qtd_arquivos <= 0:
+            pass
+        else:
+            extencoes_encontradas.append(f'{item}: {str(qtd_arquivos)}')
--- 09_Gerenciador de Arquivos/views/interface.py
+++ 09_Gerenciador de Arquivos/views/interface.py
@@ -3,37 +3,62 @@ from tkinter import Tk, StringVar
 from tkinter import ttk as ttk
 from tkinter import filedialog as fd
 from tkinter import messagebox
-
+from PIL import Image, ImageTk
 import mixins.mixins
 from mixins import *
 
 # --------------------- criando janela
 janela = Tk()
-janela.geometry('700x360')
+janela.geometry('1080x720')
 janela.title('Ebony SyS')
 janela.resizable(width=FALSE, height=FALSE)
-
+logo = PhotoImage(file=mixins.img_logo)
+janela.iconphoto(False, logo)
 style = ttk.Style(janela)
 style.theme_use("clam")
 
 # --------------------- frame header
-frame_header = Frame(janela, width=700, height=30, bg=mixins.dark2, relief='flat')
+frame_header = Frame(janela, width=1080, height=35, bg=mixins.dark2, relief='flat')
 frame_header.grid(row=0, column=0)
 
+icone_pasta = Image.open(mixins.icone_pasta)
+icone_pasta = icone_pasta.resize((30, 30))
+icone_pasta = ImageTk.PhotoImage(icone_pasta)
 
-
-
-
+iconePasta = Label(frame_header, image=icone_pasta,
+                   text='Gerenciador de Arquivos', width=1080, compound=LEFT,
+                   padx=5, relief=RAISED, anchor=CENTER, font=mixins.fonte_principal,
+                   bg=mixins.dark2, fg=mixins.cor_fonte_principal, borderwidth=0)
+iconePasta.place(x=0, y=0)
 
 # --------------------- frame main
-frame_main = Frame(janela, width=700, height=300, bg=mixins.dark1)
+frame_main = Frame(janela, width=1080, height=655, bg=mixins.dark1)
 frame_main.grid(row=1, column=0, sticky=NSEW)
 
+# --------------------- frame main/entrada
+frame_entrada = Frame(frame_main, width=350, height=655, bg=mixins.dark2)
+frame_entrada.place(x=0, y=0)
+frame_entrada.lift()
+
+label_escolher_pasta = Label(frame_entrada, text='Escolha uma pasta',
+                             anchor=CENTER, relief='flat',
+                             bg=mixins.dark2, fg=mixins.cor_fonte_principal,
+                             font=mixins.fonte_secundaria)
+label_escolher_pasta.place(x=87.5, y=30)
+
+
+
+
+# --------------------- frame main/grafico
+frame_grafico = Frame(frame_main, width=730, height=655, bg=mixins.dark1)
+frame_grafico.place(x=351, y=0)
+
+
+
+
+
 # --------------------- frame footer
-frame_footer = Frame(janela, width=700, height=30, bg=mixins.dark2)
+frame_footer = Frame(janela, width=1080, height=30, bg=mixins.dark2)
 frame_footer.grid(row=3, column=0)
-
-
-
 
 janela.mainloop()
